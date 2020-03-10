3056288fa6a41bdf365e780085fc6ac539d3aded
Van <1251787298@qq.com>
2020-03-10 17:02:26 +0800

    jdk 1.8 init

--- JDK-8/src/main/java/cn/van/jdk/eight/Application.java
+++ JDK-8/src/main/java/cn/van/jdk/eight/Application.java
@@ -5,20 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Copyright (C), 2015-2019, 风尘博客
- * 公众号 : 风尘博客
- * FileName: Application
- *
- * @author: Van
- * Date:     2019-02-22 23:17
- * Description: JDK 1.8 示例代码启动类
- * Version： V1.0
+ * @公众号： 风尘博客
+ * @Classname Application
+ * @Description 启动类
+ * @Date 2020/3/1 8:35 下午
+ * @Author by Van
  */
 @Slf4j
 @SpringBootApplication
-
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
         log.info("Application start!");
