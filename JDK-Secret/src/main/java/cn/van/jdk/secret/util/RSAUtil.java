8b543178a8145a6c71c3c6f34d81f60c87828662
vandusty <1251787298@qq.com>
2020-06-28 21:33:23 +0800

    RSA 加密工具

---
+++ JDK-Secret/src/main/java/cn/van/jdk/secret/util/RSAUtil.java
@@ -0,0 +1,102 @@
+package cn.van.jdk.secret.util;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Copyright (C), 2015-2020, 风尘博客
+ * 公众号 : 风尘博客
+ * FileName: RSAUtil
+ *
+ * @author: Van
+ * Date:     2020-06-19 20:17
+ * Description: RSA 加密工具类
+ * Version： V1.0
+ */
+public class RSAUtil {
+    /**
+     * 密钥长度 于原文长度对应 以及越长速度越慢
+     */
+    private final static int KEY_SIZE = 1024;
+    /**
+     * 用于封装随机产生的公钥与私钥
+     */
+    private static Map<Integer, String> keyMap = new HashMap<>();
+
+    /**
+     * 随机生成密钥对
+     */
+    public static Map genKeyPair() throws NoSuchAlgorithmException {
+        // KeyPairGenerator 用于生成公钥和私钥对，基于RSA算法生成对象
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        // 初始化密钥对生成器
+        keyPairGen.initialize(KEY_SIZE, new SecureRandom());
+        // 生成一个密钥对，保存在keyPair中
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        // 得到私钥
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        // 得到公钥
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        String publicKeyString = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+        // 得到私钥字符串
+        String privateKeyString = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+        // 将公钥和私钥保存到Map
+        //0表示公钥
+        keyMap.put(0, publicKeyString);
+        //1表示私钥
+        keyMap.put(1, privateKeyString);
+        return keyMap;
+    }
+
+    /**
+     * RSA公钥加密
+     *
+     * @param str       加密字符串
+     * @param publicKey 公钥
+     * @return 密文
+     * @throws Exception 加密过程中的异常信息
+     */
+    public static String encrypt(String str, String publicKey) throws Exception {
+        //base64编码的公钥
+        byte[] decoded = Base64.getDecoder().decode(publicKey);
+        RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decoded));
+        //RSA加密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+        String outStr = Base64.getEncoder().encodeToString(cipher.doFinal(str.getBytes("UTF-8")));
+        return outStr;
+    }
+
+    /**
+     * RSA私钥解密
+     *
+     * @param str        加密字符串
+     * @param privateKey 私钥
+     * @return 明文
+     * @throws Exception 解密过程中的异常信息
+     */
+    public static String decrypt(String str, String privateKey) throws Exception {
+        //64位解码加密后的字符串
+        byte[] inputByte = Base64.getDecoder().decode(str);
+        //base64编码的私钥
+        byte[] decoded = Base64.getDecoder().decode(privateKey);
+        RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
+        //RSA解密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, priKey);
+        String outStr = new String(cipher.doFinal(inputByte));
+        return outStr;
+    }
+}
+
+
--- JDK-Secret/src/test/java/cn/van/jdk/secret/util/UtilTest.java
+++ JDK-Secret/src/test/java/cn/van/jdk/secret/util/UtilTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UtilTest {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -16,11 +19,30 @@ public class UtilTest {
 
         // 输出加密后的密文
         String cipherString = AESUtil.encrypt(content, key);
-        logger.info("加密后的密文：[{}]", cipherString);
+        logger.info("AES 加密后的密文：[{}]", cipherString);
 
         // 输出解密后的明文
         String plainStr = AESUtil.decrypt(cipherString, key);
-        logger.info("解密后的明文：[{}]", plainStr);
+        logger.info("AES 解密后的明文：[{}]", plainStr);
+    }
+
+    @Test
+    public void rsaTest() throws Exception {
+        //生成公钥和私钥
+        Map<Integer, String> keyMap = RSAUtil.genKeyPair();
+        logger.info("公钥:[{}]", keyMap.get(0));
+        logger.info("私钥:[{}]", keyMap.get(1));
+
+        // 要加密的数据
+        String message = "你好，我是要发送的数据";
+
+        // 使用公钥加密
+        String messageEn = RSAUtil.encrypt(message, keyMap.get(0));
+        logger.info("RSA 加密后的密文：[{}]", messageEn);
+
+        // 使用私钥解密
+        String messageDe = RSAUtil.decrypt(messageEn, keyMap.get(1));
+        logger.info("RSA 解密后的明文：[{}]",messageDe);
     }
 
 
